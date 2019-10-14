3d8dc40b4a9a6a0bd2f3b5b2cb3837239e111229
Maria Mesimeri <mmesim@gmail.com>
2019-10-14 12:54:57 -0600

    Add files via upload

---
+++ arrival_times.m
@@ -0,0 +1,40 @@
+clear;clc; tic %start timer
+%% Paramaters
+%path to waveforms
+mydata='Array_Detection/Backprojection_SSA/DATA';
+%-------------------------------------------------------------------------
+% Parallel settings
+workers=32;                 %Set number of cores to work on a local machine
+%Define grid points
+%Minimum  - Maximum - Latitude
+minlat=38.7; maxlat=38.9;  inclat=0.01;
+%Minimum  - Maximum - Longitude 
+minlon=-112.9; maxlon=-112.7; inclon=0.01;
+%Minimum - Maximum - Depth [km]
+mindepth=2.0; maxdepth=3.0; incdepth=1.0;
+%Minimum - Maximum - Origin time [sec] [time before the detection]
+minorigin=0; maxorigin=2; incorigin=0.1;
+%--------------------------------------------------------------------------
+%Define phase 
+pha='P';   %Case sensitive
+%--------------------------------------------------------------------------
+%% 00.Setup
+parpool('local',workers); %Start parallel pool
+mydir=pwd; pdir=sprintf('%s/src/',pwd); % get working directory path
+addpath(genpath(pdir));  %add src to path (includes taup and readsac)
+%--------------------------------------------------------------------------
+%% 01. Load data (Sac files)
+disp('Loading files..')
+[y,header]=my_loadfiles(mydata);
+%% Load Velocity model
+load velocity_model.mat 
+
+%% 02. Calculate travel times
+disp('Calculate travel tcimes...')
+[tt_table,grid]=make_tt_table(pha,model,minlat, maxlat,inclat, minlon, maxlon,inclon, mindepth, maxdepth, incdepth, minorigin, maxorigin, incorigin,header);
+
+save  data.mat tt_table grid
+
+delete(gcp)
+fprintf('Elapsed time %6.2f minutes... \n',toc/60) %stop timer
+
---
+++ backprojection.m
@@ -0,0 +1,46 @@
+% Backprojection for local events                               %
+% using regional network                                        %
+%                                                               %
+% Works for one event at a time                                 %
+% Wavforms start at origin time                                 %
+% ------------------------- M.Mesimeri 10/2019 ---------------  %
+
+clear;clc; tic %start timer
+
+%% 00.Setup
+parameters %load parameter file
+parpool('local',workers); %Start parallel pool
+mydir=pwd; pdir=sprintf('%s/src/',pwd); % get working directory path
+addpath(genpath(pdir));  %add src to path (includes taup and readsac)
+%--------------------------------------------------------------------------
+%% 01. Load data (Sac files)
+display('Loading files..')
+[y,header]=my_loadfiles(mydata);
+%Velocity model
+load velocity_model.mat 
+%% 02. Preprocess data 
+display('Preprocessing...')
+y_proc=my_preprocessing(y,header(1).DELTA,type,lcorner,hcorner);
+
+%% 03. Load travel times
+load data.mat tt_table grid
+
+%% 04. Crete beam for each grid point
+display('Beam for each Grid point..')
+beam=my_beam(start,stop,tt_table,grid,y_proc,header(1).DELTA);
+
+%% 05. Measure amplitude and energy
+display('Amplitudes..')
+[ampls, all, maxgrid]=my_ampls(beam,grid);
+
+%% 06. Output #1 Figures
+display('Figures..')
+my_figure(all,minlat, maxlat,inclat, minlon, maxlon,inclon, mindepth, maxdepth, incdepth,minorigin, maxorigin, incorigin);
+
+%% 07. Output: Best Location
+fprintf('Location: %7.4f %9.4f %6.2f %3.1f \n',grid(maxgrid,:))
+
+%% 08. Shutdown parallel pool
+delete(gcp)
+fprintf('Elapsed time %6.2f minutes... \n',toc/60) %stop timer
+
